71f69592f425d9ba2eea58de0c4cb75b3a8f2578
James Durham <james.durham@vbrick.com>
2015-05-06 17:43:15 -0400

    updated logging

--- flexResizeDirective.js
+++ flexResizeDirective.js
@@ -2,7 +2,7 @@
 
 angular.module('flexResize')
 
-	.controller('flexResizeController', [ '$scope', '$element', function($scope, $element) {
+	.controller('flexResizeController', [ '$scope', '$element', '$log', function($scope, $element, $log) {
 		var control = this;
 		this.activeResize = null;
 		this.init = true;
@@ -39,7 +39,7 @@ angular.module('flexResize')
 
 		this.addContainer = function(container) {
 			this.containers.push(container);
-			console.info(this.containers);
+			$log.debug(this.containers);
 		}
 
 		function offset(element) {
@@ -55,16 +55,17 @@ angular.module('flexResize')
 
 		function resize() {
 
-			console.info("mousePos: ", control.mousePos);
-			console.info("lastPos: ", control.lastPos);
+			$log.debug("mousePos: ", control.mousePos);
+			$log.debug("lastPos: ", control.lastPos);
 
 
-			if(control.setSize) {
+			if(control.setSize || control.setSize === 0) {
 				control.lastPos = control.setSize;
+				control.containers[0].element.addClass('animate');
+			} else {
+				control.containers[0].element.removeClass('animate')
 			}
-			if(control.setSize === 0) {
-				control.lastPos = control.setSize;
-			}
+
 
 			if(control.init) {
 				_.each(control.containers, function(c){ c.initialSize = c.element.width(); } );
@@ -97,21 +98,29 @@ angular.module('flexResize')
 
 		$scope.togglePane = function(pane) {
 
+			$scope.collapsed = false;
+
 			if(control.activePane === null) {
 				control.activePane = pane;
 			}
 
 			if(control.containers[0].collapsed ) {
+				//control.containers[0].element.addClass('animate');
 				control.setSize = 384;
 				control.activePane = pane;
+
 				resize();
+				//control.containers[0].element.removeClass('animate');
 				return;
 			}
 
 			if (!control.containers[0].collapsed && control.activePane === pane) {
+				//control.containers[0].element.addClass('animate');
 				control.activePane = null;
 				control.setSize = 0;
+				$scope.collapsed = true;
 				resize();
+				//control.containers[0].element.removeClass('animate');
 				return;
 			}
 
@@ -154,6 +163,11 @@ angular.module('flexResize')
 							collapsed: attrs.collapsed || false,
 							element: element
 						};
+
+						if (attrs.collapsed) {
+							$scope.collapsed = true;
+						}
+
 						$scope.init = true;
 						control.addContainer($scope.container);
 
@@ -164,60 +178,35 @@ angular.module('flexResize')
 						});
 					}
 				}
-			}/*,
-			link: function($scope, $element) {
-				// set initial size here?
-				console.info("width: $element.width(): ", $element.width());
-
-
-
-				$scope.getElementDimensions = function() {
-					return { 'h': $element.height(), 'w': $element.width() };
-				}
-
-				$scope.$watch($scope.getElementDimensions.w, function (newValue, oldValue) {
-					console.info("newValue: ", newValue);
-
-					if ($scope.init) {
-						$scope.container.initialSize = newValue;
-						$scope.init = false;
-						console.info($scope.container);
-					}
-
-				}, true);
-
-				$element.bind('resize', function () {
-					$scope.$apply();
-				});
-			}*/
+			}
 		}
 	}])
 
-	.directive('flexResizeBar', [ function() {
+	.directive('flexResizeBar', [ '$log', function($log) {
 		return {
 			restrict: 'EAC',
 			require: '^flexResize',
 			scope: {},
-			link: function(scope, element, attrs, control) {
-				console.log('flexResizeBar element: ', element);
-				console.log('flexResizeBar element.children(): ', element.children());
+			link: function($scope, $element, attrs, control) {
+				$log.debug('flexResizeBar element: ', $element);
+				$log.debug('flexResizeBar element.children(): ', $element.children());
 
 				var root = angular.element(document.body.parentElement);
 
 
-				element.on('mousedown touchstart', function(event) {
-					control.activeResize = element;
+				$element.on('mousedown touchstart', function(event) {
+					control.activeResize = $element;
 					event.preventDefault();
 					event.stopPropagation();
 
 					root.on('mousemove touchmove', function(event) {
-						scope.$apply(angular.bind(control, control.mouseMoveHandler, event));
+						$scope.$apply(angular.bind(control, control.mouseMoveHandler, event));
 					});
 					return false;
 				})
 
 				root.on('mouseup touchend', function(event) {
-					scope.$apply(angular.bind(control, control.mouseUpHandler, event));
+					$scope.$apply(angular.bind(control, control.mouseUpHandler, event));
 					root.off('mousemove touchmove');
 				});
 			}
